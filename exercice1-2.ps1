837d00bd632cd8659f63da8be99383e603afceaf
unknown <Administrateur@LOCAL.BARCELONE.CUB.SIOPLC.FR>
2024-03-13 10:03:02 +0100

    1er commit

---
+++ exercice1-2.ps1
@@ -0,0 +1,12 @@
+﻿#=====================================================
+#
+#NAME: Exercice 1-2.ps1
+#AUTHOR: Oeillard Alexandre
+#DATE:13/03/2024
+#
+#VERSION 1.0
+#COMMENTS: Afficher les processus "svchost" en activité
+#
+#=====================================================
+
+Get-Process -ProcessName svchost* | Select-Object "Id","ProcessName"
