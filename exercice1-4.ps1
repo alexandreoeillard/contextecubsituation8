d45b8ead3783f13fc84405b5b039c318aa40073c
unknown <Administrateur@LOCAL.BARCELONE.CUB.SIOPLC.FR>
2024-03-13 10:40:24 +0100

    1er commit

---
+++ exercice1-4.ps1
@@ -0,0 +1,12 @@
+﻿#=====================================================
+#
+#NAME: Exercice 1-2.ps1
+#AUTHOR: Oeillard Alexandre
+#DATE:13/03/2024
+#
+#VERSION 1.0
+#COMMENTS: Afficher le processus choisi d'être affiché par l'utilisateur
+#
+#=====================================================
+$proc=Read-Host "Quel processus désirez vous filtrer?" 
+Get-Process -ProcessName $proc* | Format-Table Id,ProcessName, Description
