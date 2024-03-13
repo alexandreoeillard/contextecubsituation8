40ba1885828cbf31d200cb2a62a2befe62edf1c5
unknown <Administrateur@LOCAL.BARCELONE.CUB.SIOPLC.FR>
2024-03-13 11:18:19 +0100

    1er commit

---
+++ exercice1-6.ps1
@@ -0,0 +1,20 @@
+﻿#=====================================================
+#
+#NAME: Exercice 1-6.ps1
+#AUTHOR: Oeillard Alexandre
+#DATE:13/03/2024
+#
+#VERSION 1.0
+#COMMENTS: Afficher le processus choisi d'être affiché par l'utilisateur à l'aide d'une fonction
+#COMMENTS: Et enregistrer le résultat dans deux fichiers (txt, csv)
+#=====================================================
+
+function InfoProcessus($pro)
+{
+Write-Host "Affichage de l'Id, ProcessName et Description des processus contenant: $pro"
+$processus = Get-Process | Where-Object {$_.ProcessName -like "*$pro*"}
+$processus | Format-Table Id, Name, Description
+}
+$pro=Read-Host "Quel processus désirez vous filtrer?" 
+InfoProcessus($pro)
+Get-Process | Out-File -FilePath C:\Users\Administrateur.LOCAL\contextecubsituation8\Sauvegarde.txt
