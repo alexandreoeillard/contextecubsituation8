ea9b8d4ed84fbd53c1210585e0557947217db91f
unknown <Administrateur@LOCAL.BARCELONE.CUB.SIOPLC.FR>
2024-03-20 10:32:13 +0100

    1er commit

---
+++ exercice1-10.ps1
@@ -0,0 +1,34 @@
+﻿#=====================================================
+#
+#NAME: Exercice 1-9.ps1
+#AUTHOR: Oeillard Alexandre
+#DATE:20/03/2024
+#
+#VERSION 1.0
+#COMMENTS: Parcourir le fichier "services.csv"
+#COMMENTS: Et afficher en vert les services demandés
+#=====================================================
+
+$fichierservs = "C:\Users\Administrateur.LOCAL\contextecubsituation8\Services.csv"
+$filtre=Read-Host "Quel service désirez vous filtrer?" 
+
+# Vérifier si le fichier existe
+if (-Not (Test-Path $fichierservs)) {
+    Write-Host "Le fichier '$fichierservs' n'existe pas." -ForegroundColor Red
+    exit
+}
+
+# Lire le fichier CSV
+$services = Import-Csv -Path $fichierservs
+
+# Parcourir chaque service
+foreach ($serv in $services) {
+    $nomService = $serv.Name
+    $statusService = $serv.Status
+
+    # Vérifier si le nom du service contient "system"
+    if ($nomService -match "$filtre") {
+        # Changer la couleur du texte en vert pour les services contenant "system"
+        Write-Host "$nomService - $statusService" -ForegroundColor Green
+    }
+}
